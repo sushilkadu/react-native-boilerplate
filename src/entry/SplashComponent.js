aefb45e64967f8cbd2d1814783e361b23e08c7af
Ritesh <ritesh.bhagat@orangebitsindia.com>
2019-07-18 16:44:25 +0530

    Splash screen is added

--- src/entry/SplashComponent.js
+++ src/entry/SplashComponent.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
 
-import { Stylesheet, Text } from "react-native";
+import { StyleSheet, Image, Text, View } from "react-native";
+const splashscreen = require("./../../assets/icon.png");
+
+import { Container, Content } from "native-base";
 
 export default class SplashComponent extends Component {
   render() {
-    return <Text>This is a splash screen</Text>;
+    return (
+      <Container style={styles.container}>
+        <View style={styles.content}>
+          <Image source={splashscreen} />
+          <Text style={{ alignSelf: "center" }}>App Name</Text>
+        </View>
+      </Container>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  content: {
+    alignItems: "center"
+  }
+});
